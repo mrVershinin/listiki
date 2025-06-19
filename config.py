548a36577b993ee3f3ade4a3632eac2eef203cd2
mrVershinin <hello@mrvershinin.ru>
2025-06-19 17:08:46 +0000

    Добавлены серверные и доменные константы

--- config.py
+++ config.py
@@ -5,6 +5,12 @@ from pydantic import SecretStr
 class Settings(BaseSettings):
     BOT_TOKEN: SecretStr
     ID_ADMINS: list[int]
+    WEBHOOK_URL: str
+    HOST: str
+    PORT: int
+    SSL_CERTFILE: str
+    SSL_KEYFILE: str
+    WORKERS: int
 
     model_config = SettingsConfigDict(
         env_file=".env",
